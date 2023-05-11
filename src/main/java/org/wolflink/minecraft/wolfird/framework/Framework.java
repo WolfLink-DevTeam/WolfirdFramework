371218d6bdb3e1cbf582ba6e6c914605d608d3f6
MikkoAyaka <3401286177@qq.com>
2023-05-11 17:09:52 +0800

    补充一点插件的提示信息

--- src/main/java/org/wolflink/minecraft/wolfird/framework/Framework.java
+++ src/main/java/org/wolflink/minecraft/wolfird/framework/Framework.java
@@ -16,21 +16,29 @@ import java.io.File;
 public final class Framework extends SubPlugin {
 
     @Getter private static Framework INSTANCE;
+    private final BaseNotifier logger = Guice.getBean(BaseNotifier.class);
     public Framework() {
         init();
     }
-
+    private long initTime;
     @Override protected void init() {
+        logger.info("开始初始化");
+        initTime = System.currentTimeMillis();
         INSTANCE = this;
         this.saveDefaultConfig();
     }
     @Override public void onEnable() {
+        logger.info("正在加载可用模式插件...");
         loadSubPlugins("mode-plugin");
+        logger.info("正在加载可用系统插件...");
         loadSubPlugins("system-plugin");
+        logger.info("正在加载可用拓展插件...");
         loadSubPlugins("addon-plugin");
-
+        logger.info("§f初始化完成，用时 §a"+(System.currentTimeMillis()-initTime)/1000.0+" §f秒");
     }
-    @Override public void onDisable() {}
+    @Override public void onDisable() {
+        logger.info("开始卸载框架");
+    }
 
     private void loadSubPlugins(String subPluginFolderName){
         File subPluginFolder = new File(this.getDataFolder().getPath(),subPluginFolderName);
