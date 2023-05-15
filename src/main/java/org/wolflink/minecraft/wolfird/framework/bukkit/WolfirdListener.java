d6f916afe8a8b44548e5e13eef4d4c31951d9ae2
MikkoAyaka <3401286177@qq.com>
2023-05-15 20:12:05 +0800

    完善 WolfirdListener 提供了调度器任务相关的封装

--- src/main/java/org/wolflink/minecraft/wolfird/framework/bukkit/WolfirdListener.java
+++ src/main/java/org/wolflink/minecraft/wolfird/framework/bukkit/WolfirdListener.java
@@ -13,9 +13,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 // TODO 还应该提供对 SchedulerTask 相关的封装，否则在监听器禁用时这些任务仍在执行，不符合逻辑
-public class WolfirdListener implements Listener {
+public abstract class WolfirdListener implements Listener {
 
     private boolean enabled = false;
+    /**
+     * 存储该监听器涉及到的所有调度器任务，以便于禁用监听器时注销所有任务
+     */
+    private final Set<Integer> taskIdSet = new HashSet<>();
+    public void runTaskLater(Runnable runnable,long delay) {
+        taskIdSet.add(Bukkit.getScheduler().runTaskLater(Framework.getINSTANCE(),runnable,delay).getTaskId());
+    }
+    public void runTaskLaterAsync(Runnable runnable,long delay) {
+        taskIdSet.add(Bukkit.getScheduler().runTaskLaterAsynchronously(Framework.getINSTANCE(),runnable,delay).getTaskId());
+    }
+    public void runTaskTimer(Runnable runnable,long delay,long period) {
+        taskIdSet.add(Bukkit.getScheduler().runTaskTimer(Framework.getINSTANCE(),runnable,delay,period).getTaskId());
+    }
+    public void runTaskTimerAsync(Runnable runnable,long delay,long period) {
+        taskIdSet.add(Bukkit.getScheduler().runTaskTimerAsynchronously(Framework.getINSTANCE(),runnable,delay,period).getTaskId());
+    }
 
     public void setEnabled(final boolean enabled) {
         if(this.enabled == enabled)return;
@@ -26,6 +42,8 @@ public class WolfirdListener implements Listener {
         }
         // 从启用到禁用
         else {
+            // 取消所有还未完成的任务
+            taskIdSet.forEach(id -> Bukkit.getScheduler().cancelTask(id));
             Set<Class<Event>> eventClasses = new HashSet<>();
 
             for (Method method : getClass().getDeclaredMethods()) {
