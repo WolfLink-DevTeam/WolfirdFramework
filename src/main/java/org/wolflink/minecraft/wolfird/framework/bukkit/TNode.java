995bab43cfde21c5679464f3637edd482e36cf2c
MikkoAyaka <3401286177@qq.com>
2023-05-20 11:33:47 +0800

    完善 TNode 类相关方法

--- src/main/java/org/wolflink/minecraft/wolfird/framework/bukkit/TNode.java
+++ src/main/java/org/wolflink/minecraft/wolfird/framework/bukkit/TNode.java
@@ -3,10 +3,7 @@ package org.wolflink.minecraft.wolfird.framework.bukkit;
 import lombok.Getter;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 泛型节点类
@@ -15,18 +12,49 @@ import java.util.Set;
 public class TNode<T> {
     @Getter
     private final T value;
-    private final Set<TNode<T>> references = new HashSet<>();
+    private final Map<T,TNode<T>> references = new HashMap<>();
     public TNode(T value) {
         this.value = value;
     }
+    public Collection<TNode<T>> getNodes(){return references.values();}
     @Nullable
     public TNode<T> get(T value){
-        for (TNode<T> node : references) {
-            if(node.value.equals(value))return node;
-        }
-        return null;
+        return references.get(value);
     }
     public void ref(TNode<T> node){
-        references.add(node);
+        references.put(node.value,node);
+    }
+    public void del(T value){references.remove(value);}
+    public void del(TNode<T> node){
+        references.remove(node.value);
+    }
+    public void pathRef(T[] pathValues) {
+        pathRef(Arrays.stream(pathValues).map(TNode::new).toList());
+    }
+    /**
+     * 按路径层层绑定，在当前节点后按顺序追加这些节点
+     */
+    public void pathRef(Collection<TNode<T>> pathNodes) {
+        TNode<T> nowNode = this;
+        for (TNode<T> node : pathNodes) {
+            if(nowNode == null)throw new IllegalArgumentException("进行路径绑定时发现了 value 异常的节点");
+            if(nowNode.get(node.value) == null)nowNode.ref(node);
+            nowNode = nowNode.get(node.value);
+        }
+    }
+
+    /**
+     * 按路径层层解除绑定，在当前节点后按顺序追加这些节点
+     */
+    public void pathDel(T[] pathValues) {
+        if(pathValues.length == 0)return;
+        TNode<T> pathNode = get(pathValues[0]);
+        if(pathNode == null)return;
+        if(pathValues.length > 1) {
+            pathNode.pathDel(Arrays.copyOfRange(pathValues,1,pathValues.length));
+            if(pathNode.references.size() == 0)del(pathNode);
+        }
+        else del(pathValues[0]);
     }
 }
+
