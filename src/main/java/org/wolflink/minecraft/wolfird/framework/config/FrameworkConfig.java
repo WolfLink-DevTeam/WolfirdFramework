165799de30edfc84ef9cd67d0ca600c889c53202
MikkoAyaka <3401286177@qq.com>
2023-06-18 22:45:27 +0800

    Collect config files to separate folder

--- src/main/java/org/wolflink/minecraft/wolfird/framework/config/FrameworkConfig.java
+++ src/main/java/org/wolflink/minecraft/wolfird/framework/config/FrameworkConfig.java
@@ -1,11 +1,12 @@
 package org.wolflink.minecraft.wolfird.framework.config;
 
 
+import org.wolflink.minecraft.wolfird.framework.Framework;
 import org.wolflink.minecraft.wolfird.framework.ioc.Singleton;
 
 @Singleton
 public class FrameworkConfig extends YamlConfig {
     public FrameworkConfig() {
-//        super("wolfird_config");
+        super(Framework.getInstance().getDescription().getName());
     }
 }
--- src/main/java/org/wolflink/minecraft/wolfird/framework/config/YamlConfig.java
+++ src/main/java/org/wolflink/minecraft/wolfird/framework/config/YamlConfig.java
@@ -2,19 +2,27 @@ package org.wolflink.minecraft.wolfird.framework.config;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.wolflink.minecraft.wolfird.framework.Framework;
+import org.wolflink.minecraft.wolfird.framework.ioc.IOC;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 
 /**
- * 基于 Yaml 的配置文件
+ * 基于 Yaml 的配置文件，子插件声明的配置文件都会被整理到外层
  */
 public class YamlConfig extends BaseConfig {
 
+    private static final File frameworkCfgFolder = new File(Framework.getInstance().getDataFolder(),"config");
+    private final File configFile;
     FileConfiguration fileConfiguration;
 
     public YamlConfig(String configName) {
         super(configName);
+        if(!frameworkCfgFolder.exists())frameworkCfgFolder.mkdirs();
+        configFile = new File(frameworkCfgFolder,configName+".yml");
     }
 
     @Override
@@ -40,11 +48,18 @@ public class YamlConfig extends BaseConfig {
 
     @Override
     public void load() {
-        fileConfiguration = Framework.getInstance().getConfig();
+        fileConfiguration = YamlConfiguration.loadConfiguration(configFile);
     }
 
     @Override
     public void save() {
-        Framework.getInstance().saveConfig();
+        try {
+            fileConfiguration.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Framework.getInstance().getNotifier()
+                    .error("在尝试保存配置文件 "+configName+" 时出现了异常，请查看上方详细错误信息。");
+        }
+
     }
 }
