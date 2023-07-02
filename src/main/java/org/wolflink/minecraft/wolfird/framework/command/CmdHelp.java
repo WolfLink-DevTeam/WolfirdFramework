da2dcbe7afc54282467cf70f3b33169ce3c85db7
MikkoAyaka <3401286177@qq.com>
2023-07-02 16:58:31 +0800

    优化了帮助指令

--- src/main/java/org/wolflink/minecraft/wolfird/framework/command/CmdHelp.java
+++ src/main/java/org/wolflink/minecraft/wolfird/framework/command/CmdHelp.java
@@ -13,17 +13,21 @@ public class CmdHelp extends WolfirdCommand {
     @Inject
     private CommandContainer commandContainer;
 
+    private final String mainCommand;
     public CmdHelp(String mainCommand) {
         super(false, true, true, mainCommand+" help", "查看 "+mainCommand+" 插件帮助");
+        this.mainCommand = mainCommand;
     }
 
     @Override
     protected void execute(CommandSender sender, String[] args) {
         sender.sendMessage(" ");
-        sender.sendMessage("§8[ " + notifier.getPrefix() + " §8]");
+        sender.sendMessage("§8[ §a" + mainCommand + " §8] §f指令帮助");
         sender.sendMessage(" ");
         for (WolfirdCommand command : commandContainer.getCommands()) {
-            sender.sendMessage("§f" + command.getCommandTemplate() + " §8- §7" + command.getHelpMessage());
+            if(command.getCommandTemplate().startsWith(mainCommand)) {
+                sender.sendMessage("§f  " + command.getCommandTemplate() + " §8- §7" + command.getHelpMessage());
+            }
         }
         sender.sendMessage(" ");
     }
